c8919156f97b80e72351b4a43e858a78c793a553
Fyodor <fyodorsumarokov@yandex.ru>
2020-01-20 16:18:45 +0300

    Initial commit

---
+++ src/Soluction.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Soluction {
+    public static void main(String[] args) throws Exception {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(read.readLine());
+        int b = Integer.parseInt(read.readLine());
+        int c = Integer.parseInt(read.readLine());
+        int d = Integer.parseInt(read.readLine());
+        int e = Integer.parseInt(read.readLine());
+
+        int arr[] = {a, b, c, d, e};
+        bubbleSort(arr);
+    }
+
+    public static void bubbleSort(int[] arr) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int a = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = a;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++)
+            System.out.println(arr[i]);
+    }
+}
---
+++ src/sol.java
@@ -0,0 +1,54 @@
+public class sol {
+    public static class Main {
+        static int a[] = new int[15];
+        static int b[] = new int[15];
+        static int count = 1;
+
+        public static void main(String[] args) {
+            System.out.println("Изначальные данные");
+            raw();
+            sum();
+            System.out.println();
+            System.out.println("После преобразования");
+            for (int i = 0; i < a.length; i++) {
+                System.out.print(a[i] + " ");
+            }
+            System.out.println();
+            for (int j = 0; j < b.length; j++) {
+                System.out.print(b[j] + " ");
+            }
+            System.out.println();
+            System.out.println("Список задач");
+            for (int i = 0; i < 14; i++) {
+                System.out.print("Задание №" + "" + count + ": " + a[i] + "*" + b[i]);
+                System.out.println();
+                count++;
+            }
+        }
+
+        //создаем массив из случайных чисел от 2 до 9
+        static void raw() {
+            for (int i = 0; i < a.length; i++) {
+                a[i] = (int) (Math.random() * 7) + 2;
+                System.out.print(a[i] + " ");
+            }
+            System.out.println();
+            for (int i = 0; i < b.length; i++) {
+                b[i] = (int) (Math.random() * 7) + 2;
+                System.out.print(b[i] + " ");
+            }
+        }
+
+        //проверяем имеются повторяющиеся значения или нет (пример 2*3 и 3*2)
+        static void sum() {
+            for (int i = 1; i < a.length - 1; i++) {
+                for (int j = 0; j < b.length; j++) {
+                    if (((a[i] == b[j]) && (a[j] == b[i])) && (a[i] != b[i])) {
+                        a[i] = (int) (Math.random() * 7) + 2;
+                        raw();
+                    }
+                }
+            }
+        }
+    }
+}
